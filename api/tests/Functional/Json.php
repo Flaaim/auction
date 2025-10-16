6bae1d2d38cb4dfd590298b7751df008e011570d
Flaaim <flaaim@list.ru>
2025-10-16 10:42:42 +1000

    Added JSON decode test helper

---
+++ api/tests/Functional/Json.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Test\Functional;
+
+class Json
+{
+    /**
+     * @param string $data
+     * @return array
+     * @throws \JsonException
+     */
+    public static function decode(string $data): array
+    {
+        /** @var array */
+        return json_decode($data, true, 512, JSON_THROW_ON_ERROR);
+    }
+}
