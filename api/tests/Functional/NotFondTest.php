618a16982001aa58de8259e7bbd1243a199a17e6
Flaaim <flaaim@list.ru>
2025-10-16 10:38:47 +1000

    Improved not found page test

--- api/tests/Functional/NotFondTest.php
+++ api/tests/Functional/NotFondTest.php
@@ -2,13 +2,22 @@
 
 namespace Test\Functional;
 
+use DMS\PHPUnitExtensions\ArraySubset\ArraySubsetAsserts;
 class NotFondTest extends WebTestCase
 {
+    use ArraySubsetAsserts;
     public function testNotFond(): void
     {
         $response = $this->app()->handle(self::json('GET', '/test'));
 
         self::assertEquals(404, $response->getStatusCode());
+        self::assertJson($body = (string)$response->getBody());
 
+        /** @var array $data */
+        $data = json_decode($body, true, 512, JSON_THROW_ON_ERROR);
+
+        self::assertArraySubset([
+            'message' => '404 Not Found',
+        ], $data);
     }
 }
